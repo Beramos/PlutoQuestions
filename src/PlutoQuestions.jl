dae7250412d0453ec02bef1a88d78d5b0c056879
brdjaegh <bram.dejaegher@ugent.be>
2021-09-27 16:47:15 +0200

    update header

--- src/PlutoQuestions.jl
+++ src/PlutoQuestions.jl
@@ -1,3 +1,15 @@
+#=
+Created on Friday 4 December 2020
+Last update: Monday 27 September 2021
+
+@author: Bram De Jaegher
+bram.de.jaegher@gmail.com
+
+Templates heavily based on the MIT course "Computational Thinking"
+
+https://computationalthinking.mit.edu/
+=#
+
 module PlutoQuestions
 
 # Write your package code here.
