c64464e95311a92ee7336a4c6f936a5a103c0c74
brdjaegh <bram.dejaegher@ugent.be>
2021-09-27 17:51:41 +0200

    add admonition tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using PlutoQuestions
 using Test
+using Markdown
 
-@testset "PlutoQuestions.jl" begin
-    # Write your tests here.
+
+@testset "Admonition.jl" begin
+       @test typeof(PlutoQuestions.fyi("test")) <: Markdown.MD
+       @test typeof(PlutoQuestions.correct()) <: Markdown.MD
+       @test typeof(PlutoQuestions.keep_working()) <: Markdown.MD
+       @test typeof(PlutoQuestions.not_defined("test")) <: Markdown.MD
+       @test typeof(PlutoQuestions.hint("test")) <: Markdown.MD
+       @test typeof(PlutoQuestions.still_missing()) <: Markdown.MD
 end
